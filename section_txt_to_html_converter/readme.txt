1bfcbc9f5411f2ea68b454cee105c0fab9c7378d
recursive-contradiction <recursive.contradiction@proton.me>
2024-12-28 06:19:47 -0600

    template_generator and reading_2 outline
    #2
    
    made the template_generator
    described usage in the readme
    used the template_generator to make the outline for reading_2.html
    
    Still need to use the converter to put the sections into reading_2.html

--- section_txt_to_html_converter/readme.txt
+++ section_txt_to_html_converter/readme.txt
@@ -1,9 +1,25 @@
+Intro:
+There are two tools in this folder, template_generator.py and converter.py.
+template_generator is used to create the reading_x.html template (where x represents the class number) at the beginning
+of each pdf to html conversion. The produced file will have sections labeled SECTION_CONTENT_HERE which are then filled
+in via the converter.
+
+To use the template_generator you will have to define two variables at the start of the file. A more detailed
+description of those variables is in the comments of the file itself.
+
+To use the converter:
 1. Download the class pdf you're trying to convert sections in
-2. use https://www.pdf2go.com/pdf-to-word to convert it to a word document (this will make copying and pasting sections easier so we avoid missing characters, which happens frequently when copy/pasting directly from pdfs)
-3. If you don't have word, upload the new .docx to your google drive so you can view it therein. Otherwise just view it in word.
+2. use https://www.pdf2go.com/pdf-to-word to convert it to a word document (this will make copying and pasting sections 
+    easier so we avoid missing characters, which happens frequently when copy/pasting directly from pdfs)
+3. If you don't have word, upload the new .docx to your google drive so you can view it therein. Otherwise just view it
+    in word.
 4. Open the .docx
-5. Copy and paste the complete content of a section (taking care to omit page numbers, headers/footers, etc) into the variable `original_section` in the converter.py
-6. Open a cmd and navigate to the section_txt_to_html_converter directory then run `python converter.py`. This will produce output.
-7. Copy and paste the output into the reading_x.html section you were converting. *TIP: at least in VSCODE, pasting at the very beginning of the line (before any whitespace) will automatically respsect indentation.
-8. Repeat steps 5 through 7 until you've finished all sections of the reading. *TIP: you can use `clear` in cmd to get rid of old lines to reduce clutter and eye-strain.
+5. Copy and paste the complete content of a section (taking care to omit page numbers, headers/footers, etc) into the
+    variable `original_section` in the converter.py
+6. Open a cmd and navigate to the section_txt_to_html_converter directory then run `python converter.py`. This will
+    produce output.
+7. Copy and paste the output into the reading_x.html section you were converting. *TIP: at least in VSCODE, pasting at
+    the very beginning of the line (before any whitespace) will automatically respsect indentation.
+8. Repeat steps 5 through 7 until you've finished all sections of the reading. *TIP: you can use `clear` in cmd to get
+    rid of old lines to reduce clutter and eye-strain.
 9. Repeat steps 1 through 8 for all class pdfs you're converting.
---
+++ section_txt_to_html_converter/template_generator.py
@@ -0,0 +1,132 @@
+# You must define the outputName and the classDict
+
+# outputName should be of the format "reading_1.html", "reading_2.html", etc
+outputName = 'reading_2.html'
+
+# {articleName:{'date':"Month Day, Year", 'author':"authorName", 'sections':["SectionTitle1, sectionTitle2"]}}
+classDict = {
+    "Proletarians and Communists / Chapter 2 of the Communist Manifesto" : \
+        {
+            'date': 'February 21, 1848', 
+            'author': 'Karl Marx and Friedrich Engels',
+            'sections' : []
+        },
+    "The Weapon of Theory" : \
+        {
+            'date': 'January 1966',
+            'author': 'Amilcar Cabral',
+            'sections': []
+        },
+    "Socialist Reconstruction, Introduction" : \
+        {
+            'date': 'October 2022',
+            'author': 'Party for Socialism and Liberation',
+            'sections':
+                [
+                    "WORKERS MAKE THE WORLD RUN—WORKERS SHOULD RUN THE WORLD",
+                    "CLIMATE CHANGE IS CLASS STRUGGLE",
+                    "SOCIALISM IS POSSIBLE AND NECESSARY",
+                    "MEANINGFUL WORK AND MORE FREE TIME",
+                    "A BETTER FUTURE UNDER SOCIALISM"
+                ]
+        }
+}
+
+
+
+###### DONT EDIT PAST HERE ###### DONT EDIT PAST HERE ###### DONT EDIT PAST HERE ######
+
+start = """<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Articles with Collapsible Sidebar</title>
+  <link rel="stylesheet" href="reading_style.css">
+</head>
+<body>
+
+  <div id="sidebar">
+    <h2>Outline</h2>
+    <ul>"""
+
+outline_dynamic_articleName = lambda articleID, articleName: f"""
+      <li><a href="#{articleID}"><h1>{articleName}</h1></a></li>"""
+
+outline_dynamic_section_start = """
+        <ul>"""
+outline_dynamic_section = lambda sectionID, sectionName: f"""
+          <li><a href="#{sectionID}"><h2>{sectionName}</h2></a></li>"""
+outline_dynamic_section_end = f"""
+        </ul>"""
+
+
+middle = """
+    </ul>
+  </div>
+  <button id="toggle-btn">☰</button>
+
+  <div id="content">"""
+
+
+content_dynamic_articleName_date_author = lambda articleID, articleName, date, author: f"""
+    <article id="{articleID}">
+      <h1>{articleName}</h1>
+      <hr>
+      <div class = "publish-date">{date}</div>
+      <div class="author">{author}</div>
+      <hr>
+SECTION_CONTENT_HERE"""
+
+content_dynamic_section = lambda sectionID, sectionName: f"""
+      <section id="{sectionID}">
+        <h2>{sectionName}</h2>
+SECTION_CONTENT_HERE
+      </section>"""
+
+content_article_end = """
+    </article>
+"""
+
+end = """
+  </div>
+  <script>
+    // JavaScript for Collapsible Sidebar
+    const sidebar = document.getElementById('sidebar');
+    const toggleBtn = document.getElementById('toggle-btn');
+
+    toggleBtn.addEventListener('click', () => {
+      sidebar.classList.toggle('collapsed');
+    });
+  </script>
+</body>
+</html>"""
+
+output = start
+
+# create outline 
+for articleNum, (articleName, dateAuthorSections) in enumerate(classDict.items(), start=1):
+    articleID = f'a{articleNum}'
+    output += outline_dynamic_articleName(articleID, articleName)
+    if len(dateAuthorSections['sections']) > 0: output += outline_dynamic_section_start
+    for sectionNum, sectionName in enumerate(dateAuthorSections['sections'], start=1):
+        sectionID = f'{articleID}s{sectionNum}'
+        output += outline_dynamic_section(sectionID, sectionName)
+    if len(dateAuthorSections['sections']) > 0: output += outline_dynamic_section_end
+
+output += middle
+
+# create content 
+for articleNum, (articleName, dateAuthorSections) in enumerate(classDict.items(), start=1):
+    articleID = f'a{articleNum}'
+    output += content_dynamic_articleName_date_author(articleID, articleName, dateAuthorSections['date'], dateAuthorSections['author'])
+    for sectionNum, sectionName in enumerate(dateAuthorSections['sections'], start=1):
+        output += content_dynamic_section(sectionID, sectionName)
+    output += content_article_end
+
+output += end
+
+with open(outputName, 'w', encoding='utf-8') as f:
+    f.write(output)
+
+
